c1a948c6bc6e3ff9e2d36d960d325bc43235ff4d
jalankampusindonesia <jalankampusindonesia@gmail.com>
2018-11-23 05:20:02 +0700

    TA-WEB2

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Dashbord Admin Jalan Kampus</title>
+  <title>KELOMPOK 3</title>
    <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="{{asset('css/admin.css')}}">
@@ -19,9 +19,9 @@
     <!-- Logo -->
     <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini">ADM</span>
+      <span class="logo-mini">K3</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg">Dashboard Admin</span>
+      <span class="logo-lg">Kelompok 3</span>
     </a>
 
     <!-- Header Navbar -->
